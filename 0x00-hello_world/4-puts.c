c91df635824acbc2a979003d839994b29a201afe
Belayneh <belayaa2013@gmail.com>
2023-02-16 12:58:49 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #!/bin/bash
 #include <stdio.h>
+/**
+ * puts also do like printf
+ * but not all
+ */
 int main(void)
 {
-puts("programming is like building a multilingual puzzle");
+puts("\"programming is like building a multilingual puzzle");
 return (0);
 } 

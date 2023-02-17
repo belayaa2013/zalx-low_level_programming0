278f6b32b41a4c6220447d525bf822be6175ebb6
Belayneh <belayaa2013@gmail.com>
2023-02-16 16:55:00 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main is a function that prints a line with print function
+ * retun: 0(success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");

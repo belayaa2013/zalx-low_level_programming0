57088b3729dd7449b520f34b603fd0492bd67540
Belayneh <belayaa2013@gmail.com>
2023-02-16 16:34:40 -0800

    data sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int main(void)
+/* main funcrion prints out the size of data */
 {
 	char a;
 	int b;
 	long int c;
 	long long int d;
 	float f;
-
-	printf("size of a char: %lu byte(s)\n",(unsigned long)sizeof(a));
-	printf("size of an int: %lu byte(s)\n",(unsigned long)sizeof(b));
-	printf("size of long int: %lu byte(s)\n",(unsigned long)sizeof(c));
-        printf("size of long long int: %lu byte(s)\n",(unsigned long)sizeof(d));
-	printf("size of float: %lu byte(s)\n",(unsigned long)sizeof(f));
-	return(0);
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
 }

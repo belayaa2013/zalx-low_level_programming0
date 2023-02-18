134cf5dcc6d906feb1d5192e3a10fe88575f9375
Belayneh <belayaa2013@gmail.com>
2023-02-17 18:34:09 -0800

    numbers with comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - a function lists hexadecimal
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i + '0');
+			if (i != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+	}
+	return (0);
+}
+

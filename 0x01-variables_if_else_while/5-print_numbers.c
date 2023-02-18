125df438a442cb88e3ea8a101caeb66312780dce
Belayneh <belayaa2013@gmail.com>
2023-02-17 17:01:59 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a function prints from 0 t 9
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	printf("%d", i);
+	printf("\n");
+	return (0);
+}

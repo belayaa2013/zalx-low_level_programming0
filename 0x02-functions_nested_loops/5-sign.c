633216210b7b458ff32f05e6ebb825d6e6195aed
Belayneh <belayaa2013@gmail.com>
2023-02-21 16:54:36 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_sign - function that lables a number as positive negative or zero
+ * print_sign - A function that lables a number as positive negative or zero
  * @n: a number to be checked
  * Return: 0 1 0r -1
  */
@@ -9,23 +9,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-	/*	_putchar(',');*/
-	/*	_putchar(' ');*/
 		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-	/*	_putchar(',');*/
-	/*	_putchar(' ');*/
 		return (0);
 	}
-	else if (n < 0)
+	else
 	{
-	/*	_putchar('-');*/
-	/*	_putchar(',');*/
-	/*	_putchar(' ' );*/
-		_putchar('-');
+			_putchar('-');
 		return (-1);
 	}
 	return ('\n');

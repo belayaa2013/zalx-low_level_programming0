f1ae0d680bed725e970125aa069ad472d9de4bcc
Belayneh <belayaa2013@gmail.com>
2023-02-22 09:02:54 -0800

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,7 +6,7 @@
  */
 void print_to_98(int n)
 {
-	int i;
+	int i,j;
 
 	if (n <= 98)
 	{
@@ -16,6 +16,16 @@ void print_to_98(int n)
 				printf("%d, ", i);
 			else if (i == 98)
 				printf("%d\n", i);
+		}
+	}
+	else if (n <= 111)
+	{
+		for (j = n; j <= 111 ; j++)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d", j);
 		}
 	}
 }

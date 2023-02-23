693a80f2c9d8c8a30ee0d770ae87c62a736c5e86
Belayneh <belayaa2013@gmail.com>
2023-02-23 14:26:07 -0800

    digit or not

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - a function that identifies a digit or not
+ * @c: a parameter to be checked
+ * Return: 1(success)
+ */
+int _isdigit(int c)
+{
+	if (c == '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}

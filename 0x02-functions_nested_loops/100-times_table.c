a97b92959f76dddbf0e2592efb051e76b4c39fe4
Belayneh <belayaa2013@gmail.com>
2023-02-25 16:48:22 -0800

    time table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,39 +1,44 @@
 #include "main.h"
 /**
- * void print_times_table - a function which prints n times table
+ * print_times_table - a function which prints n times table
  * @n: an integral a multiple of two numbers
  * Return: succcess
  */
 void print_times_table(int n)
 {
-	int l, m;
+	int l, m, k;
 
 	for (n = 0 ; n <= 15; n++)
 	{
 		for (l = 0 ; l <= 15; l++)
 		{
 			m = n * l;
-			if (l == 0)
+
+			if (m < 10)
 			{
 				_putchar(m + '0');
-			}
-			if (m < 10 && l != 0)
-			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(n + '0');
+				_putchar(' ');
 			}
 			else if (m >= 10 && m < 100)
 			{
-				_putchar(',');
-				_putchar(' ');
 				_putchar((m / 10) + '0');
 				_putchar((m % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 			}
 			else if (m >= 100)
 			{
-
+				k = m % 100;
+				_putchar((m / 100) + '0');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}

ab570417d5a09064e3f9f6608b7a90ad73e7be3a
Belayneh <belayaa2013@gmail.com>
2023-02-27 12:21:42 -0800

    updates the value

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* Reset_to_98 - a function updates the value of 98
+* reset_to_98 - a function updates the value of 98
 * @n: a parameter to be asigned
 * Return: its success
 */

8f399df0b691829769ba823a01f73dd024794f41
Belayneh <belayaa2013@gmail.com>
2023-02-27 18:41:26 -0800

    reversing a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,9 +12,9 @@ void rev_string(char *s)
 	while (s[i++])
 		c++;
 	for (i = c - 1; i >= c / 2; i--)
-		{
-			ch = s[i];
-			s[i] = s[c - i - 1];
-			s[c - i - 1] = ch;
-		}
+	{
+		ch = s[i];
+		s[i] = s[c - i - 1];
+		s[c - i - 1] = ch;
+	}
 }

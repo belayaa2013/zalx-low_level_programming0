26d21d13120c07e069d0edce99dd504fa7d7f06d
Belayneh <belayaa2013@gmail.com>
2023-02-27 17:38:52 -0800

    second half of the string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,16 +8,18 @@ void puts_half(char *str)
 	int i = 0, n;
 
 	while (str[i] != '\0')
+	{
 		i++;
+	}
 	if (i % 2 == 1)
-		{
-			n = (i - 1) / 2;
-			n += 1;
-		}
-		else
-		{
-			n = i / 2;
-		}
+	{
+		n = (i - 1) / 2;
+		n += 1;
+	}
+	else
+	{
+		n = i / 2;
+	}
 	for (; n < i; n++)
 	{
 		_putchar(str[n]);

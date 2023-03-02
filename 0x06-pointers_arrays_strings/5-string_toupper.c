e90469b81a0fefd3217e5d1abb550c7b4b34e85f
Belayneh <belayaa2013@gmail.com>
2023-03-01 23:06:18 -0800

    uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
  * string_toupper - a function that convert to upper case
+ * @n: the poiter
  * Return: elements of the array in uppercase
  */
-char *string_toupper(char *)
+char *string_toupper(char *n)
 {
 	int i;
 
 	for (i = 0; n[i] != '\0'; n++)
 	{
 		if (n[i] >= 'a' && n[i] <= 'z')
-			n[i] = n[i] - 22;
+			n[i] = n[i] - 32;
 	}
 	return (n);
 }

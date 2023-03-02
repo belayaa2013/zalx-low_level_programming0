f4563f8ada5cb5ddb8b917e5f919bf7a021f8232
Belayneh <belayaa2013@gmail.com>
2023-03-02 13:22:41 -0800

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,25 +6,21 @@
   */
 char *cap_string(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; !(str[i] >= 'a' && str[i] <= 'z'); i++)
+	while (str[i])
 	{
-	if (str[i - 1] == ' ' ||
-	str[i - 1] == '\t' ||
-	str[i - 1] == '\n' ||
-	str[i - 1] == ',' ||
-	str[i - 1] == ';' ||
-	str[i - 1] == '.' ||
-	str[i - 1] == '!' ||
-	str[i - 1] == '?' ||
-	str[i - 1] == '"' ||
-	str[i - 1] == '(' ||
-	str[i - 1] == ')' ||
-	str[i - 1] == '{' ||
-	str[i - 1] == '}' ||
-	i == 0)
-	str[i] -= 32;
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+		if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+				str[i - 1] == ',' || str[i - 1] == ';' ||
+				str[i - 1] == '.' || str[i - 1] == '!' ||
+				str[i - 1] == '?' || str[i - 1] == '"' ||
+				str[i - 1] == '(' || str[i - 1] == ')' ||
+				str[i - 1] == '{' || str[i - 1] == '}' ||
+				i == 0)
+			str[i] -= 32;
+		i++;
 	}
 	return (str);
 }

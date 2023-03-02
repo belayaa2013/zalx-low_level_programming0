cfa3f3be49ee1744c44bbbbeaa7362cc55099e61
Belayneh <belayaa2013@gmail.com>
2023-03-02 13:47:55 -0800

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,17 +1,23 @@
-char *rot13(char *)
+#include "main.h"
+/**
+ * rot13 _ a function that replaces a letter with 13th letter
+ * @s: the array os string to be replaced
+ * Return: return the string
+ */
+char *rot13(char *s)
 {
 	int i, j;
 
-	chat data1[] = 	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char norm[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; s[i] ! '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (s[i] == data1[j])
+			if (s[i] == norm[j])
 			{
-				s[i] = datarot[j];
+				s[i] = rot[j];
 				break;
 			}
 		}

804594b0068f024c8d9a77b3eff69461407e29f6
Belayneh <belayaa2013@gmail.com>
2023-03-06 18:56:22 -0800

    add the diagonals

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,6 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - the function that prints the sum of two diagonals
+ * @a: the diagonal point
+ * @size: size of the diagonal
+ */
 void print_diagsums(int *a, int size)
 {
-	int i, sum =0, sum1 = 0;
+	int i, sum = 0, sum1 = 0;
 
 	for (i = 0; i < size; i++)
 	{

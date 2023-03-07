25fe20215cd7b5202567367c2f3537eb1ed50878
Belayneh <belayaa2013@gmail.com>
2023-03-06 18:23:28 -0800

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,17 +8,19 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0;
+	int i;
 
-/*	while (*s)
+	while (*s)
 	{
+		i = 0;
+
 		while (accept[i])
-		{*/
+		{
 			if (*s == accept[i])
 				return (s);
 			i++;
-	/*	}*/
+		}
 		s++;
-/*	}*/
+	}
 	return ('\0');
 }

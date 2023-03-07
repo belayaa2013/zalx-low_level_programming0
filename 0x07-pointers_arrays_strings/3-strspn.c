32a9fa426f0ee7e93827880896cbdbafb0042563
Belayneh <belayaa2013@gmail.com>
2023-03-06 18:17:41 -0800

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,21 +7,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0;
+	int i;
 	unsigned int n = 0;
 
 	while (*s)
 	{
-		for (; accept[i]; i++)
+		for (i = 0; accept[i]; i++)
 		{
 			if (*s == accept[i])
 			{
 				n++;
+				i++;
 				break;
 			}
 			else if (accept[i + 1] == '\0')
 			{
-				return (0);
+				return (n);
 			}
 		}
 		s++;

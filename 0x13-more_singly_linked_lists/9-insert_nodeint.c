7007176cce1c60e87cd9eb4516e496f608d2c23d
Belayneh <belayaa2013@gmail.com>
2023-04-04 09:47:07 -0700

    insert @ given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,36 +11,34 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
- unsigned int i;
- listint_t *new;
- listint_t *temp = *head;
+	unsigned int i;
 
- new = malloc(sizeof(listint_t));
- if (!new || !head)
- return (NULL);
+	listint_t *newnode;
 
- new->n = n;
- new->next = NULL;
+	listint_t *temp = *head;
 
- if (idx == 0)
- {
- new->next = *head;
- *head = new;
- return (new);
- }
+	newnode = malloc(sizeof(listint_t));
 
- for (i = 0; temp && i < idx; i++)
- {
- if (i == idx - 1)
- {
- new->next = temp->next;
- temp->next = new;
- return (new);
- }
- else
- temp = temp->next;
- }
-
- return (NULL);
+	if (!newnode || !head)
+		return (NULL);
+	newnode->n = n;
+	newnode->next = NULL;
+	if (idx == 0)
+	{
+		newnode->next = *head;
+		*head = newnode;
+		return (newnode);
+	}
+	for (i = 0; temp && i < idx; i++)
+	{
+		if (i == idx - 1)
+		{
+			newnode->next = temp->next;
+			temp->next = newnode;
+			return (newnode);
+		}
+		else
+			temp = temp->next;
+	}
+	return (NULL);
 }
-
